051f0ac9e10378a1a3e053ad1d554690de00acde
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-01-01 17:23:06 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/lucasdavi/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.lucasdavi.workshopmongodb.config;
+
+import com.lucasdavi.workshopmongodb.models.User;
+import com.lucasdavi.workshopmongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        List<User> users = Arrays.asList(maria, alex, bob);
+
+        userRepository.saveAll(users);
+    }
+}
