3e45842efd8bba8be506464fc82da35e834361cc
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-01-01 23:21:05 -0300

    Obtendo um usuário por id

--- src/main/java/com/lucasdavi/workshopmongodb/controllers/UserController.java
+++ src/main/java/com/lucasdavi/workshopmongodb/controllers/UserController.java
@@ -5,14 +5,17 @@ import com.lucasdavi.workshopmongodb.models.User;
 import com.lucasdavi.workshopmongodb.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,6 +30,12 @@ public class UserController {
         List<User> userList = userService.findAll();
         List<UserDTO> userDTOList = userList.stream().map(user -> new UserDTO(user.getName(), user.getEmail())).collect(Collectors.toList());
         return ResponseEntity.ok().body(userDTOList);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> findById(@PathVariable(value = "id") String id) {
+        Optional<User> user = Optional.ofNullable(userService.findById(id));
+        return ResponseEntity.status(HttpStatus.OK).body(user.get());
     }
 
 }
--- src/main/java/com/lucasdavi/workshopmongodb/services/UserService.java
+++ src/main/java/com/lucasdavi/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.lucasdavi.workshopmongodb.services;
 
 import com.lucasdavi.workshopmongodb.models.User;
 import com.lucasdavi.workshopmongodb.repositories.UserRepository;
+import com.lucasdavi.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 }
